8cf2295257c755d25493422aa954a21997d52af8
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2025-03-18 12:44:40 -0500

    module 1 assignment

---
+++ module_1/basics_syntax.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+class User{
+
+  int? id;
+  String? name;
+  String? email;
+
+  // User(){
+  //   id = 1;
+  //   name = "abc";
+  //   email = "abc@gmail.com";
+  // }
+
+  User({this.id, this.name, this.email});
+
+  Map<dynamic, dynamic> toJson() {
+
+    Map<dynamic, dynamic> map = new Map<dynamic, dynamic>();
+    map["id"] = id;
+    map["name"] = name;
+    map["email"] = email;
+
+    return map;  
+  
+  }
+  
+}
+
+void main(){
+
+User user = new User(id: 32, name: "John");
+var jsonOutput = user.toJson();
+print(jsonOutput.runtimeType);
+print(jsonOutput);
+
+User user2 = new User(id: 90, name: "amond",  email: "delta@outlook.com");
+print("user2 : ${user2.toJson()}");
+}
